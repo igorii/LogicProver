cc6590f1a0188c5d234671bf51de147174b537d0
timthornton <t.thornton6@gmail.com>
2014-09-15 14:14:55 -0400

    Add longer example

--- Main.hs
+++ Main.hs
@@ -9,5 +9,10 @@ invalidProp = PAnd (PVar "p") (PNegate (PVar "p"))
 validProp2 = PCond (PVar "p") (PVar "p")
 invalidProp2 = PCond (PVar "p") (PVar "q")
 
+-- [(A implies B) and (A implies C)] implies [A implies (B and C)]
+longValid = PCond (PAnd (PCond (PVar "A") (PVar "B")) (PCond (PVar "A") (PVar "C"))) 
+    (PCond (PVar "A") (PAnd (PVar "B") (PVar "C")))
+
 main :: IO ()
-main = putStrLn . show $ [True, True, False, False] == map isValid [validProp, validProp2, invalidProp, invalidProp2]
+main = putStrLn . show $ [True, True, True, False, False] == 
+    map isValid [longValid, validProp, validProp2, invalidProp, invalidProp2]
