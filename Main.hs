71a807ccf2e3bc04cc4d061fcb592af26239fadc
Tim Thornton <t.thornton6@gmail.com>
2014-11-14 01:44:57 -0500

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -3,15 +3,15 @@ module Main where
 import LogicProver.Lang
 import LogicProver.Prover
 
-validProp = POr (PVar "p") (PNegate (PVar "p"))
-invalidProp = PAnd (PVar "p") (PNegate (PVar "p"))
+validProp = (PVar "p") `POr` (PNegate (PVar "p"))
+invalidProp = (PVar "p") `PAnd` (PNegate (PVar "p"))
 
-validProp2 = PCond (PVar "p") (PVar "p")
-invalidProp2 = PCond (PVar "p") (PVar "q")
+validProp2 = (PVar "p") `PCond` (PVar "p")
+invalidProp2 = (PVar "p") `PCond` (PVar "q")
 
 -- [(A implies B) and (A implies C)] implies [A implies (B and C)]
-longValid = PCond (PAnd (PCond (PVar "A") (PVar "B")) (PCond (PVar "A") (PVar "C"))) 
-    (PCond (PVar "A") (PAnd (PVar "B") (PVar "C")))
+longValid = (((PVar "A") `PCond` (PVar "B")) `PAnd` ((PVar "A") `PCond` (PVar "C"))) `PCond`
+    ((PVar "A") `PCond` ((PVar "B") `PAnd` (PVar "C")))
 
 main :: IO ()
 main = putStrLn . show $ [True, True, True, False, False] ==
