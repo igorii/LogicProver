ce60f6e61374b4a3db04ff36ee4506ed6821c377
timthornton <t.thornton6@gmail.com>
2014-11-15 16:29:02 -0500

    Finally add parsing

---
+++ LogicProver/Parse.hs
@@ -0,0 +1,53 @@
+module LogicProver.Parse (parseString, parseFile, prop) where
+
+import Control.Monad
+import Text.Parsec
+import Text.Parsec.String
+import qualified Text.Parsec.Token as Token
+import Text.Parsec.Language
+import Control.Applicative ((<*))
+import Text.Parsec.Expr
+
+import LogicProver.Lang
+
+languageDef =
+  emptyDef { Token.commentLine = "--"
+           , Token.identStart = letter
+           , Token.identLetter = alphaNum
+           , Token.reservedOpNames = [ "!"
+                                     , "and"
+                                     , "or"
+                                     , "impl"
+                                     ]
+           , Token.reservedNames = [ "and"
+                                   , "or"
+                                   , "impl"
+                                   ] 
+            }
+
+lexer = Token.makeTokenParser languageDef
+
+identifier = Token.identifier lexer
+reserved = Token.reserved lexer
+reservedOp = Token.reservedOp lexer
+parens = Token.parens lexer
+
+opTable = [ [Prefix (reservedOp "!" >> return PNegate)]
+          , [Infix  (reservedOp "and" >> return PAnd) AssocLeft]
+          , [Infix  (reservedOp "or" >> return POr) AssocLeft]
+          , [Infix  (reservedOp "impl" >> return PCond) AssocLeft]
+          ]
+
+prop :: Parser Prop
+prop = buildExpressionParser opTable term
+
+term =  parens prop 
+    <|> liftM PVar identifier
+
+parseString :: Parser Prop -> String -> Either ParseError Prop
+parseString e s = parse (e <* eof) "" s
+parseFile :: Parser Prop -> FilePath -> Either ParseError Prop
+parseFile e f = parse (e <* eof) "" f
+
+
+
--- Main.hs
+++ Main.hs
@@ -2,17 +2,10 @@ module Main where
 
 import LogicProver.Lang
 import LogicProver.Prover
+import LogicProver.Parse
 
-validProp = POr (PVar "p") (PNegate (PVar "p"))
-invalidProp = PAnd (PVar "p") (PNegate (PVar "p"))
-
-validProp2 = PCond (PVar "p") (PVar "p")
-invalidProp2 = PCond (PVar "p") (PVar "q")
-
--- [(A implies B) and (A implies C)] implies [A implies (B and C)]
-longValid = PCond (PAnd (PCond (PVar "A") (PVar "B")) (PCond (PVar "A") (PVar "C"))) 
-    (PCond (PVar "A") (PAnd (PVar "B") (PVar "C")))
-
+validProp = "p impl p"
 main :: IO ()
-main = putStrLn . show $ [True, True, True, False, False] ==
-    map isValid [longValid, validProp, validProp2, invalidProp, invalidProp2]
+main = putStrLn . show $ case parseString prop validProp of 
+    Right x -> isValid x
+    Left err -> False
