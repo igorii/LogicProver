df93cd41875e191dbfff08f24ab766b5344414f8
timthornton <t.thornton6@gmail.com>
2014-11-16 18:02:43 -0500

    Evaluate command line arguments

--- Main.hs
+++ Main.hs
@@ -1,12 +1,21 @@
 module Main where
 
+import System.Environment
+import Data.List
+
 import LogicProver.Lang
 import LogicProver.Prover
 import LogicProver.Parse
 
 validProp = "p impl p"
 
+eval str =
+    show $ case parseString prop str of
+        Right x  -> isValid x
+        Left err -> False
+
 main :: IO ()
-main = putStrLn . show $ case parseString prop validProp of
-    Right x  -> isValid x
-    Left err -> False
+main = do 
+    ps <- getArgs
+    mapM (\x -> putStrLn $ eval x) ps
+    return ()
